eaf93ac3777c7cbb46842a9d559e13637929bcaa
datboi_212 <datboi212mc@gmail.com>
2025-02-17 00:58:28 -0800

    made the title appear all the time because it looks cool

--- src/app/title.tsx
+++ src/app/title.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
-const Title = () => { 
-    
+const Title = ({ className }: { className?: string }) => { 
   const scrollToNextSection = () => {
     window.scrollTo({
       top: window.innerHeight,
@@ -11,16 +10,16 @@ const Title = () => {
 
   return (
     <div>
-        <div className="title-container w-screen h-screen flex flex-col justify-center items-center text-white gap-6">
+        <div className={`title-container w-screen h-screen flex flex-col justify-center items-center text-white gap-6 ${className}`}>
             <div className="rounded-lg w-auto h-auto bg-black bg-opacity-10 flex flex-col justify-center items-center p-5">
                 <h1 className="text-6xl md:text-9xl">B E A C O N</h1>
                 <h2 className="text-xl md:text-2xl subtitle">Making lives more accessible.</h2>
             </div>
             {/* <button 
-                    onClick={scrollToNextSection}
-                    className="mt-8 px-6 py-3 bg-white text-black rounded-full hover:bg-opacity-80 transition-all duration-300"
-                >
-                    Learn More ↓
+                onClick={scrollToNextSection}
+                className="mt-8 px-6 py-3 bg-white text-black rounded-full hover:bg-opacity-80 transition-all duration-300"
+            >
+                Learn More ↓
             </button> */}
         </div>
     </div>
